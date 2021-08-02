35d463f8ec0d1ca2253bb90787f168774ffdbff1
redamoon <yuuki60824@gmail.com>
2021-08-02 23:49:57 +0900

    add 記事公開時のみデプロイに変更

--- plugins/VercelDeployHooks/lib/VercelDeployHooks/Callbacks.pm
+++ plugins/VercelDeployHooks/lib/VercelDeployHooks/Callbacks.pm
@@ -4,13 +4,18 @@ use strict;
 use MT;
 
 use VercelDeployHooks::Util qw( plugin instance request doLog );
+use MT::EntryStatus qw(:all);
 
 # 記事の保存時
 sub post_save_entry {
     my $app = MT->instance;
     my $blog_id = $app->param('blog_id');
-    doLog($blog_id);
-    request('vercel_deploy_hooks_production', $blog_id, 'save_production', $app, 'error_production');
+    my ($cb, $a, $obj, $org_obj) = @_;
+    my $status = status_text($obj->status);
+    doLog($status);
+    unless ($status eq 'Draft') {
+        request('vercel_deploy_hooks_production', $blog_id, 'save_production', $app, 'error_production');
+    }
     $app->call_return;
 }
 
