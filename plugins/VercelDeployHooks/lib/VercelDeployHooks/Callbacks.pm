153f7602f07f2693d57086904118226cb32a34cc
redamoon <yuuki60824@gmail.com>
2021-08-03 10:59:09 +0900

    fix 不要な記述削除

--- plugins/VercelDeployHooks/lib/VercelDeployHooks/Callbacks.pm
+++ plugins/VercelDeployHooks/lib/VercelDeployHooks/Callbacks.pm
@@ -1,6 +1,5 @@
 package VercelDeployHooks::Callbacks;
 use strict;
-
 use MT;
 
 use VercelDeployHooks::Util qw( plugin instance request doLog );
@@ -12,7 +11,6 @@ sub post_save_entry {
     my $blog_id = $app->param('blog_id');
     my ($cb, $a, $obj, $org_obj) = @_;
     my $status = status_text($obj->status);
-    doLog($status);
     unless ($status eq 'Draft') {
         request('vercel_deploy_hooks_production', $blog_id, 'save_production', $app, 'error_production');
     }
