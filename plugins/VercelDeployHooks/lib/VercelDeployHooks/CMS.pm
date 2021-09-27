795652ce0b670e4c85dafb8fb7fcdf36559f3cfd
redamoon <yuuki60824@gmail.com>
2021-09-28 02:45:25 +0900

    add デプロイ先のパス ユーザ パスワードでプレビューへリダイレクトする設定を対応

--- plugins/VercelDeployHooks/lib/VercelDeployHooks/CMS.pm
+++ plugins/VercelDeployHooks/lib/VercelDeployHooks/CMS.pm
@@ -18,7 +18,9 @@ sub setting {
     $app->add_breadcrumb($plugin->translate('Vercel Deploy Hooks Plugin'));
     my %param = (
         vercel_deploy_hooks_production => $plugin->get_config_value('vercel_deploy_hooks_production', 'blog:' . $blog_id),
-        vercel_deploy_hooks_develop    => $plugin->get_config_value('vercel_deploy_hooks_develop', 'blog:' . $blog_id)
+        vercel_deploy_hooks_develop    => $plugin->get_config_value('vercel_deploy_hooks_develop', 'blog:' . $blog_id),
+        preview_url                    => $plugin->get_config_value('preview_url', 'blog:' . $blog_id),
+        blog_path                      => $plugin->get_config_value('blog_path', 'blog', $blog_id)
     );
     return instance->load_tmpl('vercel_deploy.tmpl', \%param);
 }
@@ -40,6 +42,26 @@ sub develop {
     my $blog_id;
     $blog_id = $blog->id;
     request('vercel_deploy_hooks_develop', $blog_id, 'save_develop', $app, 'error_develop');
+    $app->call_return;
+}
+
+# プレビューURLを保存する
+sub preview {
+    my $app = shift;
+    my $blog = $app->blog;
+    my $blog_id;
+    $blog_id = $blog->id;
+    request('preview_url', $blog_id, 'save_preview', $app, 'error_preview');
+    $app->call_return;
+}
+
+# ブログパスを保存する
+sub blog_path {
+    my $app = shift;
+    my $blog = $app->blog;
+    my $blog_id;
+    $blog_id = $blog->id;
+    request('blog_path', $blog_id, 'save_path', $app, 'error_path');
     $app->call_return;
 }
 
--- plugins/VercelDeployHooks/lib/VercelDeployHooks/Callbacks.pm
+++ plugins/VercelDeployHooks/lib/VercelDeployHooks/Callbacks.pm
@@ -5,7 +5,7 @@ use MT;
 use VercelDeployHooks::Util qw( plugin instance request doLog );
 use MT::EntryStatus qw(:all);
 
-# 記事の保存時
+# 記事の公開時
 sub post_save_entry {
     my $app = MT->instance;
     my $blog_id = $app->param('blog_id');
@@ -15,6 +15,22 @@ sub post_save_entry {
         request('vercel_deploy_hooks_production', $blog_id, 'save_production', $app, 'error_production');
     }
     $app->call_return;
+}
+
+# 記事のプレビュー時
+sub post_preview_entry {
+    my $instance = MT->instance;
+    my $blog_id = $instance->param('blog_id');
+    my $plugin = plugin();
+
+    my ($cd, $app, $obj, $orig_obj) = @_;
+    my $q = $app->param;
+    my $entry_id = $q->param('id');
+    my $preview_url = $plugin->get_config_value('preview_url', 'blog:' . $blog_id);
+    my $path = $plugin->get_config_value('blog_path', 'blog:' . $blog_id);
+    my $author = $app->user->name;
+    my $password = $app->user->api_password;
+    return $instance->redirect($preview_url . '?path=' . $path . $entry_id . '&user=' . $author . '&pass=' . $password)
 }
 
 1;
